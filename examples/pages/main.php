6fd6a6650ca5c0835b29cf6981c849a6b28ea466
Andy Daniel Navarro Taño <andaniel05@gmail.com>
2020-04-20 16:04:36 -0400

    Temp

--- examples/pages/main.php
+++ examples/pages/main.php
@@ -4,5 +4,8 @@ use ThenFriends\ComposedAdminLte\Layout;
 
 $page = new Layout;
 $page->setBasePath('/assets/');
+$page->setTitle('My Title');
+$page->setContentTitle('My Content Title');
+$page->setContentDescription('My Content Description');
 
 echo $page;
