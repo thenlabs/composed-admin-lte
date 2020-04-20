b52538c3e5702ae4567eed94d466513e628495be
Andy Daniel Navarro Taño <andaniel05@gmail.com>
2020-04-20 08:00:26 -0400

    Temp

--- examples/pages/main.php
+++ examples/pages/main.php
@@ -5,38 +5,9 @@
  * You should remove or edit it.
  */
 
-use ThenLabs\ComposedViews\AbstractView;
-use ThenLabs\ComposedViews\Annotation\Data;
+use ThenFriends\ComposedAdminLte\Layout;
 
-$page = new class extends AbstractView {
-    /**
-     * @Data
-     */
-    protected $title;
-
-    /**
-     * @Data
-     */
-    protected $content;
-
-    protected function getView(array $data = []): string
-    {
-        return <<<HTML
-<!DOCTYPE html>
-<html lang="en">
-<head>
-    <meta charset="UTF-8">
-    <title>{$data['title']}</title>
-</head>
-<body>
-    <h1>{$data['content']}</h1>
-</body>
-</html>
-HTML;
-    }
-};
-
-$page->setTitle('Homepage');
-$page->setContent('Hello World');
+$page = new Layout;
+$page->setBasePath('/assets/');
 
 echo $page;
